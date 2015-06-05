5483030fa0577f198262c388756da5e0924bfc75
Joseph <Joseph@192.168.1.143>
2015-06-05 16:56:15 -0700

    github first submit

---
+++ src/dealertrack2/DealerTrack2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dealertrack2;
+
+/**
+ *
+ * @author Joseph
+ */
+public class DealerTrack2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
