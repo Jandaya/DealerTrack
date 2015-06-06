4c7044ed53ed611d41c6f8cfefdd5cb4c5c9fd61
Joseph <Joseph@192.168.1.143>
2015-06-05 17:30:31 -0700

    Connected java to mysql Database with a test table

--- src/dealertrack2/DealerTrack2.java
+++ src/dealertrack2/DealerTrack2.java
@@ -1,9 +1,10 @@
+import java.sql.*;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dealertrack2;
+//package dealertrack2;
 
 /**
  *
@@ -14,8 +15,40 @@ public class DealerTrack2 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // TODO code application logic here
+        Connection myConnection = null;
+        Statement myStatement = null;
+        ResultSet myResult = null;
+        
+        String user = "root";
+        String pass = "";
+        
+        try {
+            myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/DealerTrack", user, pass);
+            
+            myStatement = myConnection.createStatement();
+            
+            myResult = myStatement.executeQuery("select * from keylog");
+            while(myResult.next()){
+                System.out.println(myResult.getString("stock"));
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally{
+            if(myResult != null){
+                myResult.close();
+            }
+            if(myStatement != null){
+                myStatement.close();
+            }
+            if(myConnection != null){
+                myConnection.close();
+            }
+            
+        }
+        
+        
     }
     
 }
